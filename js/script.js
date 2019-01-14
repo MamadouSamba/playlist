b95563ec13d977b366543174fb7fdbfb51ba1534
Mamadou Samba <mamadou02@outlook.com>
2019-01-14 23:01:58 +0000

    Playlist

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Billie Jean", "Eye of the Tiger", "Immigrant Song", "Flying without Wings"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-
+var images=["https://img.discogs.com/va-9ph723y7ayF0ayZgjv_LLxbM=/fit-in/600x601/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-309649-1201009566.jpeg.jpg",
+"https://images-na.ssl-images-amazon.com/images/I/61Gm3XmbNpL._SX355_.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrVMIkQrLKlkOjGt3eN72E_PGDi3DSZAYUsdbYEZmnXiO04anJbg","https://images-na.ssl-images-amazon.com/images/I/41QW6YD3SRL.jpg"]
+var artists=["Michael Jackson","Survivor","Led Zeppelin","Ruben Studdard"]
+var length=[294,246,147,224]
+var links=["https://www.youtube.com/watch?v=Kr4EQDVETuA","https://www.youtube.com/watch?v=ktQ8FbzniLU","https://www.youtube.com/watch?v=y8OtzJtp-EM","https://www.youtube.com/watch?v=QhqUt3CAJUI"]
 
 
 function displaySongInfo(){
