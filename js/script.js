2b2f2256cd8ae344acd3866abd0a2216b4b54b7a
Mamadou Samba <mamadou02@outlook.com>
2019-02-04 23:06:16 +0000

    Complete-ish

--- js/script.js
+++ js/script.js
@@ -7,25 +7,36 @@ var songs = ["Billie Jean", "Eye of the Tiger", "Immigrant Song", "Flying withou
 var images=["https://img.discogs.com/va-9ph723y7ayF0ayZgjv_LLxbM=/fit-in/600x601/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-309649-1201009566.jpeg.jpg",
 "https://images-na.ssl-images-amazon.com/images/I/61Gm3XmbNpL._SX355_.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrVMIkQrLKlkOjGt3eN72E_PGDi3DSZAYUsdbYEZmnXiO04anJbg","https://images-na.ssl-images-amazon.com/images/I/41QW6YD3SRL.jpg"]
 var artists=["Michael Jackson","Survivor","Led Zeppelin","Ruben Studdard"]
-var length=[294,246,147,224]
+var lengths=[294,246,147,224]
 var links=["https://www.youtube.com/watch?v=Kr4EQDVETuA","https://www.youtube.com/watch?v=ktQ8FbzniLU","https://www.youtube.com/watch?v=y8OtzJtp-EM","https://www.youtube.com/watch?v=QhqUt3CAJUI"]
 
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
 songs.forEach(function(x){
-    $("#songs").append("<p> " + x + "!</p>");
+    $("#songs").append("<p> " + x + ".</p>");
 });
     images.forEach(function(x){
         $("#images").append("<img src= " + x + ">");
     
 });
-
+        artists.forEach(function(x){
+            $("#artists").append("<p>" + x + "</p>");
+        });
+            lengths.forEach(function(x){
+                $("#length").append("<p>" + x + "</p>");
+        }); 
+        links.forEach(function(x) {
+            $("#links").append("<a href=" + x + ">Watch</a>");
+        });
 }
 function emptySongInfo(){
     $("#songs").empty();
     // Use jQuery to empty all of the remaining divs
-
+    $("#images").empty();
+    $("#artists").empty();
+    $("#lengths").empty();
+    $("#links").empty();
 
 }
 
@@ -33,6 +44,17 @@ function emptySongInfo(){
 function addSongInfo(){
     // BELOW write the code to add new items to each of the arrays.
 
+var newSong=$("#song").val();
+    songs.push(newSong);
+var imageUrl=$("#image").val();
+    images.push(imageUrl);
+var artistName=$("#artist").val();
+    artists.push(artistName);
+var songLength=$("#length").val();
+    lengths.push(songLength);
+var songLink=$("#link").val();
+    links.push(songLink);
+    
 
 }
 
